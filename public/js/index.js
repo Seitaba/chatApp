275707c82fe3e079ff7944f83ca4bfe7351a69d5
Tabakovic <seid.tabakovic@sogeti.com>
2018-02-22 10:23:09 +0100

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,18 +2,12 @@ var socket = io();
 
     socket.on('connect', function() {
         console.log('Connected to server !');
-
-        socket.emit('createMessage', {
-            from: "Seid",
-            message: "Hello from client !"
-        });
     });
-
-    socket.on('newMessage', function(message){
-        console.log("message from server: ", message);
-    });
-
 
     socket.on('disconnect', function() {
         console.log('Disconnected from server !');
+    });
+
+    socket.on('newMessage', function(message){
+        console.log("newMessage", message);
     });
