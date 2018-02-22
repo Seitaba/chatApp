280ed6c5f8ea31631f4b12832a8908315caabe35
Tabakovic <seid.tabakovic@sogeti.com>
2018-02-22 13:59:12 +0100

    utils generateMessage

---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+var expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Toto';
+        var text = 'some text';
+        var message = generateMessage(from, text);
+
+        expect(message).toInclude({from, text});
+    });
+});
